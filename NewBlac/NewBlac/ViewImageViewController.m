b5fd335e386babbb4a71aedffec09be14e72d94b
Ahryun Moon <ansdkfus13@gmail.com>
2014-01-03 17:18:18 -0800

    Shows preview image

--- NewBlac/NewBlac/ViewImageViewController.m
+++ NewBlac/NewBlac/ViewImageViewController.m
@@ -19,16 +19,15 @@
 - (void)setPhoto:(UIImage *)photo
 {
     _photo = photo;
-    [self displayPhoto:photo];
 }
 
-- (void)displayPhoto:(UIImage *)image
+- (void)displayPhoto
 {
-    if (image) {
-        NSLog(@"Image exists and the image size is %f x %f\n", image.size.width, image.size.height);
+    if (self.photo) {
+        NSLog(@"Image exists and the image size is %f x %f\n", self.photo.size.width, self.photo.size.height);
         // image is a UIImage init alloced with NSData
-        self.croppedImageView.image = image;
-        self.croppedImageView.frame = CGRectMake(0, 0, image.size.width, image.size.height);
+        self.croppedImageView.image = self.photo;
+        self.croppedImageView.frame = CGRectMake(0, 0, self.photo.size.width, self.photo.size.height);
     }
 }
 
@@ -36,6 +35,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    NSLog(@"I'm in view did load\n");
+    [self displayPhoto];
     
 }
 
