377c8f378a5b45671e5f62b245cf2a48d93b542d
Ahryun Moon <ansdkfus13@gmail.com>
2014-01-24 16:34:33 -0800

    Need to register button view and layout and layout attribute

---
+++ NewBlac/NewBlac/CollectionViewButtonsView.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewButtonsView.h
+//  NewBlac
+//
+//  Created by Ahryun Moon on 1/24/14.
+//  Copyright (c) 2014 Ahryun Moon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewButtonsView : UICollectionReusableView
+
+@end
---
+++ NewBlac/NewBlac/CollectionViewButtonsView.m
@@ -0,0 +1,31 @@
+//
+//  CollectionViewButtonsView.m
+//  NewBlac
+//
+//  Created by Ahryun Moon on 1/24/14.
+//  Copyright (c) 2014 Ahryun Moon. All rights reserved.
+//
+
+#import "CollectionViewButtonsView.h"
+
+@implementation CollectionViewButtonsView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
