0b4cd91e74ce4799a276d987a5b85ec7bf0606d3
Ahryun Moon <ansdkfus13@gmail.com>
2014-03-13 17:12:30 -0700

    format change only in app delegate

--- NewBlac/NewBlac/newBlacAppDelegate.m
+++ NewBlac/NewBlac/newBlacAppDelegate.m
@@ -24,6 +24,7 @@
     [Parse setApplicationId:@"gsiksRJI1A3BsNSiKYeN8e4AatcFQVeUeTlOQhvJ"
                   clientKey:@"fowYql3KcQxl0lGVumCEshYXgiKJpjUEQ2h5v9oa"];
     [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+    [PFFacebookUtils initializeFacebook];
     return YES;
 }
 							
@@ -47,11 +48,16 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [FBAppCall handleDidBecomeActiveWithSession:[PFFacebookUtils session]];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    return [FBAppCall handleOpenURL:url sourceApplication:sourceApplication withSession:[PFFacebookUtils session]];
 }
 
 @end
